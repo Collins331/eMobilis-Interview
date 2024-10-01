3fd368c190beebc0806a5f199e9bc426d95e94b9
Collins <ochiengcollinsoduor2@gmail.com>
2024-10-01 12:05:30 +0300

    course

--- course/urls.py
+++ course/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_courses, name='courses'),
+    path('create/', views.create_course, name='create_course'),
     path('<int:course_id>/', views.course_detail, name='course'),
     path('<int:course_id>/update', views.update_course, name='update'),
     path('<int:course_id>/delete', views.delete_course, name='delete'),
